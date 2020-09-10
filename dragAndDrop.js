3edf36be475afebd09672242d3e460f9e6dd36da
AlExSeMaK <aleksejj219952@gmail.com>
2020-09-10 19:14:50 +0300

    commit

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -2,7 +2,13 @@ let firstDiv = document.getElementById('firstDiv');
 let secondDiv = document.getElementById('secondDiv');
 let dropDiv = document.getElementById('dropDiv');
 let form = document.forms.namedItem('form');
-console.log(form)
+let phone = document.getElementById('phone');
+let email = document.getElementById('email');
+let name = document.getElementById('name');
+let phoneError = document.getElementById('phoneError');
+let emailError = document.getElementById('emailError');
+let nameError = document.getElementById('nameError');
+
 
 function dragStart( event ) {
     event.dataTransfer.setData("Text", event.target.id);
@@ -25,5 +31,7 @@ function drop( event ) {
     let data = event.dataTransfer.getData("Text");
     event.target.appendChild( document.getElementById( data ));
 }
+
+
 
 
